5913896e7f9304ecd8db96a7357e4b77bdb8bcf4
Cong Ma <macong1989@gmail.com>
2021-11-18 20:45:26 +1100

    Add Template Method pattern.

---
+++ TemplateMethod/TempldateMethod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    public abstract class TemplateMethod
+    {
+        public void TemplateMethodCall()
+        {
+            Console.WriteLine("Start sequence of methods in the template");
+            PrimitiveMethodA();
+            ConcreteMethod();
+            Hook();
+            Console.WriteLine("End of template method");
+        }
+
+        /// <summary>
+        /// This is the primitive method. It should not be overridden by concrete classes
+        /// </summary>
+        private void PrimitiveMethodA()
+        {
+            Console.WriteLine("This is PrimitiveMethodA.");
+        }
+
+        /// <summary>
+        /// Concrete classes must override this abstract method
+        /// </summary>
+        protected abstract void ConcreteMethod();
+
+        /// <summary>
+        /// The hook method is doing nothing by default, although concrete classes can enrich/override it if necessary
+        /// </summary>
+        protected void Hook()
+        {
+        }
+    }
+
+    public class ConcreteTemplateMethod : TemplateMethod
+    {
+        protected sealed override void ConcreteMethod()
+        {
+            Console.WriteLine($"This is ConcreteMethod in {nameof(ConcreteTemplateMethod)} class");
+        }
+    }
+}
---
+++ TemplateMethod/readme.md
@@ -0,0 +1,2 @@
+﻿# Summary
+The Template Method defines the steps of an algorithm and allows subclasses to provide the information for one or more steps.
